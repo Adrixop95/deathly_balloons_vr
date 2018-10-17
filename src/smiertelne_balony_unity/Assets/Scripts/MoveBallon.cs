30000bb70d9d38c2a18d9ea6e8868640a319a93f
KamilKarpinski <kamil.k3@outlook.com>
2018-10-17 22:59:31 +0200

    Udoskonalenie systemów ruchu obiektów
    * Uproszczenie funkcji
    * Opis funkcji
    * Funkcja obracania postaci w kierunku kolejnego punktu na trasie

--- src/smiertelne_balony_unity/Assets/Scripts/MoveBallon.cs
+++ src/smiertelne_balony_unity/Assets/Scripts/MoveBallon.cs
@@ -2,59 +2,44 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MoveBallon : MonoBehaviour
-{
+public class MoveBallon : MonoBehaviour {
 
-    public Transform[] waypoints;
+    public Transform target;
     public float speed = 1f;
-    public float rotation = 10f;
     public int current_position;
 
-    // Use this for initialization
-    void Start()
-    {
+    /// <summary> Funkcja Inicjalizacji </summary>
+    void Start() {
 
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        moveCamera();
+    /// <summary> Funkcja Update </summary>
+    void Update() {
+        moveBallon();
     }
 
-    void OnTriggerEnter(Collider collision)
-    {
+    /// <summary> Wykrycie kolizji i innym obiektem </summary>
+    /// <param name="collision"> Collider innego obiektu </param>
+    void OnTriggerEnter(Collider collision) {
         if (collision.gameObject.name == "Camera") {
             Destroy(this.gameObject);
         }
     }
 
-        /*
-    void OnCollisionEnter(Collision col)
-    {
-        if (col.gameObject.name == "Camera")
-        {
-            Destroy(this.gameObject);
-        }
-    }
-    */
-
-    public void moveCamera()
-    {
-        if (!objectPrecisionPositioning(transform, waypoints[current_position], 0.1f))
-        {
-            var pos = Vector3.MoveTowards(transform.position, waypoints[current_position].position, speed * Time.deltaTime);
+    /// <summary> Ruch obiektu do określonego miejsca </summary>
+    public void moveBallon() {
+        if (!objectPrecisionPositioning(transform, target, 0.1f)) {
+            var pos = Vector3.MoveTowards(transform.position, target.position, speed * Time.deltaTime);
             GetComponent<Rigidbody>().MovePosition(pos);
         }
-        else
-        {
-            current_position = (current_position + 1) % waypoints.Length;
-        }
-
     }
 
-    public static bool objectPrecisionPositioning(Transform subject, Transform target, float accuracy)
-    {
+    /// <summary> Sprawdzenie czy postać (obiekt) znajduje się w granicach punktu docelowego </summary>
+    /// <param name="subject"> Obiekt, postać </param>
+    /// <param name="target"> Obiekt, punkt docelowy </param>
+    /// <param name="accuracy"> Tolerancja odległości </param>
+    /// <returns> Zwraca informacje czy obiekt jest już w polu docelowym </returns>
+    public static bool objectPrecisionPositioning(Transform subject, Transform target, float accuracy) {
         float subjectX = subject.position.x;
         float subjectZ = subject.position.z;
         float targetX = target.position.x;
--- src/smiertelne_balony_unity/Assets/Scripts/MoveCharacter.cs
+++ src/smiertelne_balony_unity/Assets/Scripts/MoveCharacter.cs
@@ -4,35 +4,54 @@ using UnityEngine;
 
 public class MoveCharacter : MonoBehaviour {
 
-    public Transform[] waypoints;
+    public Transform path;
     public float speed = 1f;
-    public float rotation = 10f;
+    public float rotation = 1f;
     public int current_position;
+    public bool infinite_loop = true;
 
-	// Use this for initialization
+	/// <summary> Funkcja Inicjalizacji </summary>
 	void Start () {
 		
 	}
 	
-	// Update is called once per frame
+	/// <summary> Funkcja Update </summary>
 	void Update () {
+        rotateCamera();
         moveCamera();
 	}
 
+    /// <summary> Ruch postaci po określonej ścieżce z Waypointów </summary>
     public void moveCamera() {
-        if ( !objectPrecisionPositioning( transform, waypoints[current_position], 0.1f ) ) {
-            //var _direction = (waypoints[current_position].position - transform.position).normalized;
-            //var _lookRotation = Quaternion.LookRotation(_direction);
-            //transform.rotation = Quaternion.Slerp(transform.rotation, _lookRotation, Time.deltaTime * rotation * Time.deltaTime);
-            var pos = Vector3.MoveTowards(transform.position, waypoints[current_position].position, speed * Time.deltaTime);
+        if ( !objectPrecisionPositioning( transform, path.GetChild(current_position), 0.1f ) ) {
+            var pos = Vector3.MoveTowards( transform.position, path.GetChild(current_position).position, speed * Time.deltaTime );
             GetComponent<Rigidbody>().MovePosition(pos);
         } else {
-            current_position = (current_position + 1) % waypoints.Length;
+            if (infinite_loop) {
+                current_position = (current_position + 1) % path.childCount;
+            } else { 
+                if (current_position < path.childCount-1) { current_position++; }
+            }
         }
-        
     }
 
-    public static bool objectPrecisionPositioning( Transform subject, Transform target, float accuracy ) {
+    /// <summary> Obrót postaci w kierunku następnego Waypointa </summary>
+    public void rotateCamera() {
+        Vector3 player = transform.position;
+        Vector3 target = path.GetChild(current_position).position;
+        player.y = 0;
+        target.y = 0;
+        var direction = (target - player).normalized;
+        var lookRotation = Quaternion.LookRotation(direction);
+        transform.rotation = Quaternion.Slerp( transform.rotation, lookRotation, rotation * Time.deltaTime );
+    }
+
+    /// <summary> Sprawdzenie czy postać (obiekt) znajduje się w granicach punktu docelowego </summary>
+    /// <param name="subject"> Obiekt, postać </param>
+    /// <param name="target"> Obiekt, punkt docelowy </param>
+    /// <param name="accuracy"> Tolerancja odległości </param>
+    /// <returns> Zwraca informacje czy obiekt jest już w polu docelowym </returns>
+    public bool objectPrecisionPositioning( Transform subject, Transform target, float accuracy ) {
 		float	subjectX		=	subject.position.x;
 		float	subjectZ		=	subject.position.z;
 		float	targetX			=	target.position.x;
@@ -42,5 +61,16 @@ public class MoveCharacter : MonoBehaviour {
 		bool	positioningZ	=	( subjectZ < targetZ + accuracy && subjectZ > targetZ - accuracy ) ? true : false;
 		return	( positioningX && positioningZ );
 	}
+
+    public bool objectPrecisionRotating( Vector3 subject, Vector3 target, float accuracy ) {
+		float	subjectX		=	subject.x;
+		float	subjectZ		=	subject.z;
+		float	targetX			=	target.x;
+		float	targetZ			=	target.z;
+
+		bool	positioningX	=	( subjectX < targetX + accuracy && subjectX > targetX - accuracy ) ? true : false;
+		bool	positioningZ	=	( subjectZ < targetZ + accuracy && subjectZ > targetZ - accuracy ) ? true : false;
+		return	( positioningX && positioningZ );
+    }
 
 }
