af6458abafd9072271a25700d737827172526362
adrixop95 <adrixop95@me.com>
2018-10-24 17:34:32 +0200

    merge | spawnowanie balonów

--- src/smiertelne_balony_unity/Assets/Scripts/MotherBallon.cs
+++ src/smiertelne_balony_unity/Assets/Scripts/MotherBallon.cs
@@ -22,10 +22,15 @@ public class MotherBallon : MonoBehaviour {
         // Sprawdzenie czy został stworzony
         if (!isCreated) {
             // Tworzenie 2 balonów 
-            for (int i = 0; i < 2; i++) {
+            for (int i = 0; i < 3; i++) {
                 // Tworzenie klona z parametrem dziedziczonym z MoveBallon.cs
                 GameObject go = (GameObject) Instantiate( prefabSpawn );
-                go.transform.position = transform.position;
+                //go.GetComponent<CapsuleCollider>().isTrigger = true;
+
+                int minus = ((Random.Range(0, 1) > 0 ? -1 : 1));
+                Vector3 adder = new Vector3((1f + Random.Range(0f, 2f)) * minus, (1f + Random.Range(0f, 2f)) * minus, (1f + Random.Range(0f, 2f)) * minus);
+
+                go.transform.position = transform.position + adder;
                 go.transform.parent = transform.parent;
                 go.GetComponent<MoveBallon>().target = GetComponent<MoveBallon>().target;
             }
