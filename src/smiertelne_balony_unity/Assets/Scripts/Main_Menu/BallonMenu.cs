a6179e0db9d761d30efc9cea4a16563b7e3fe17f
KamilKarpinski <kamil.k3@outlook.com>
2018-11-07 03:06:19 +0100

    Dodanie menu głównego
    * Dodanie sceny menu głównego
    * Dodanie zmodyfikowanego prefabu kamery
    * Dodanie zmodyfikowanych prefabów balonów pod menu
    * Nowe dwa skrypty do obsługi menu i poruszania balonów w menu

---
+++ src/smiertelne_balony_unity/Assets/Scripts/Main_Menu/BallonMenu.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Vector3Static {
+    public Vector3Static( Vector3 v3, bool[] vl ) {
+        vector3 = v3;
+        vector_lock = vl;
+    }
+    public Vector3 vector3;
+    public bool[] vector_lock = new bool[3] { false, true, false };
+}
+
+public class BallonMenu : MonoBehaviour {
+
+    public Vector3Static[] points;
+    public float speed = 0.1f;
+    private int current_position = 0;
+	
+	void Update () {
+        Vector3Static v3s = points[current_position];
+        Vector3 target = new Vector3(
+            v3s.vector_lock[0] ? v3s.vector3.x : transform.position.x,
+            v3s.vector_lock[1] ? v3s.vector3.y : transform.position.y,
+            v3s.vector_lock[2] ? v3s.vector3.z : transform.position.z
+        );
+
+		if ( !ObjectPrecisionPositioning( transform.position, target, 0.1f ) ) {
+            var pos = Vector3.MoveTowards( transform.position, target, speed * Time.deltaTime );
+            GetComponent<Rigidbody>().MovePosition(pos);
+        } else {
+            current_position = (current_position + 1) % points.Length;
+        }
+	}
+
+    /// <summary> Sprawdzenie czy postać (obiekt) znajduje się w granicach punktu docelowego </summary>
+    /// <param name="subject"> Aktualna pozycja obiektu przemieszczającego się </param>
+    /// <param name="target"> Położenie punktu docelowego </param>
+    /// <param name="accuracy"> Tolerancja odległości </param>
+    /// <returns> Zwraca informacje czy obiekt jest już w polu docelowym </returns>
+    public static bool ObjectPrecisionPositioning( Vector3 subject, Vector3 target, float accuracy ) {
+        float subjectX = subject.x;
+        float subjectY = subject.y;
+        float subjectZ = subject.z;
+        float targetX = target.x;
+        float targetY = target.y;
+        float targetZ = target.z;
+
+        bool positioningX = (subjectX < targetX + accuracy && subjectX > targetX - accuracy) ? true : false;
+        bool positioningY = (subjectY < targetY + accuracy && subjectY > targetY - accuracy) ? true : false;
+        bool positioningZ = (subjectZ < targetZ + accuracy && subjectZ > targetZ - accuracy) ? true : false;
+        return (positioningX && positioningY && positioningZ);
+    }
+
+}
---
+++ src/smiertelne_balony_unity/Assets/Scripts/Main_Menu/MainMenu.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour {
+
+    public CameraRaycast raycaster;
+    public bool start_pause = true;
+    public float raycast_time = 5f;
+
+    private GameObject new_game;
+    private GameObject levels;
+    private GameObject exit;
+    private GameObject[] info = new GameObject[4];
+
+    public string[,] menu_info = new string[,] {
+        { "Fabuła\ni Narracja", "Fabuła i narracja\nLaura Dymarczyk" },
+        { "Twórca\nAssetów", "Twórca Obiektów 3D\nAgata Dziurka" },
+        { "Programista\n1", "Programista 1\n Kamil Karpiński" },
+        { "Programista\n2", "Programista 1\n Adrian Rupala" }
+    };
+
+	// Use this for initialization
+	void Start () {
+		new_game = transform.GetChild(0).gameObject;
+        levels = transform.GetChild(1).gameObject;
+        exit = transform.GetChild(2).gameObject;
+
+        for( int i = 0; i < 4; i++ ) {
+            info[i] = transform.GetChild(i+3).gameObject;
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if ( raycaster.GetSelectedActive() >= raycast_time ) {
+            GameObject raycastObject = raycaster.GetSelectedObject();
+            if ( raycastObject == null ) { return; }
+            if ( raycastObject.name == "Start Balloon" ) {
+                SceneManager.LoadScene( "Level_1", LoadSceneMode.Single );
+
+            } else if ( raycastObject.name == "Levels Balloon" ) {
+                // ... // ... // ... //
+
+            } else if ( raycastObject.name == "Exit Balloon" ) {
+                Application.Quit();
+
+            } else if ( raycastObject.name == "Info_AD Balloon" ) {
+                ChangeTexts( raycastObject, 0, true );
+
+            } else if ( raycastObject.name == "Info_LD Balloon" ) {
+                ChangeTexts( raycastObject, 1, true );
+
+            } else if ( raycastObject.name == "Info_KK Balloon" ) {
+                ChangeTexts( raycastObject, 2, true );
+
+            } else if ( raycastObject.name == "Info_AR Balloon" ) {
+                ChangeTexts( raycastObject, 3, true );
+
+            } else {
+                for ( int i = 0; i < 4; i++ ) {
+                    ChangeTexts( raycastObject, i, true );
+                }
+            }
+        }
+	}
+
+    /// <summary> Zmień tekst balona na podstawie tablicy publicznej </summary>
+    /// <param name="obj"> Obiekt balonu </param>
+    /// <param name="textID"> Indeks tablicy zewnętrznej </param>
+    /// <param name="show"> Prawda dla tekstu rozwiniętego </param>
+    private void ChangeTexts( GameObject obj, int textID, bool show ) {
+        var child = obj.transform.GetChild(0);
+        TextMesh text_mesh = child.GetComponent<TextMesh>();
+        text_mesh.text = show ? menu_info[textID,1] : menu_info[textID,0];
+    }
+}
