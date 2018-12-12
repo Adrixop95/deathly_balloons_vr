3e5144cae12177f638553ce5b0fab51f314d7d45
KamilKarpinski <kamil.k3@outlook.com>
2018-12-12 13:21:07 +0100

    Path no longer visible
    Poprawki w Main Menu i Final Menu

--- src/smiertelne_balony_unity/Assets/Scripts/Main_Menu/MainMenu.cs
+++ src/smiertelne_balony_unity/Assets/Scripts/Main_Menu/MainMenu.cs
@@ -14,13 +14,6 @@ public class MainMenu : MonoBehaviour {
     private GameObject exit;
     private GameObject[] info = new GameObject[4];
 
-    public string[,] menu_info = new string[,] {
-        { "Fabuła\ni Narracja", "Fabuła i narracja\nLaura Dymarczyk" },
-        { "Twórca\nAssetów", "Twórca Obiektów 3D\nAgata Dziurka" },
-        { "Programista\n1", "Programista 1\n Kamil Karpiński" },
-        { "Programista\n2", "Programista 1\n Adrian Rupala" }
-    };
-
 	// Use this for initialization
 	void Start () {
 		new_game = transform.GetChild(0).gameObject;
@@ -40,39 +33,14 @@ public class MainMenu : MonoBehaviour {
             if ( raycastObject.name == "Start Balloon" ) {
                 SceneManager.LoadScene( "Level_01", LoadSceneMode.Single );
 
-            } else if ( raycastObject.name == "Levels Balloon" ) {
-                // ... // ... // ... //
+            } else if ( raycastObject.name == "Load Balloon" ) {
+                SceneManager.LoadScene( "Level_02", LoadSceneMode.Single );
 
             } else if ( raycastObject.name == "Exit Balloon" ) {
                 Application.Quit();
 
-            } else if ( raycastObject.name == "Info_AD Balloon" ) {
-                ChangeTexts( raycastObject, 0, true );
-
-            } else if ( raycastObject.name == "Info_LD Balloon" ) {
-                ChangeTexts( raycastObject, 1, true );
-
-            } else if ( raycastObject.name == "Info_KK Balloon" ) {
-                ChangeTexts( raycastObject, 2, true );
-
-            } else if ( raycastObject.name == "Info_AR Balloon" ) {
-                ChangeTexts( raycastObject, 3, true );
-
-            } else {
-                for ( int i = 0; i < 4; i++ ) {
-                    ChangeTexts( raycastObject, i, true );
-                }
             }
         }
 	}
 
-    /// <summary> Zmień tekst balona na podstawie tablicy publicznej </summary>
-    /// <param name="obj"> Obiekt balonu </param>
-    /// <param name="textID"> Indeks tablicy zewnętrznej </param>
-    /// <param name="show"> Prawda dla tekstu rozwiniętego </param>
-    private void ChangeTexts( GameObject obj, int textID, bool show ) {
-        var child = obj.transform.GetChild(0);
-        TextMesh text_mesh = child.GetComponent<TextMesh>();
-        text_mesh.text = show ? menu_info[textID,1] : menu_info[textID,0];
-    }
 }
